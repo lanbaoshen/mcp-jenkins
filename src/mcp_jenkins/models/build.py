23951c7beec01fd124d41fa5b55cf963ddbbae46
lanbaoshen <lanbaoshen@icloud.com>
2025-06-13 11:39:42 +0800

    Update build.py to allow nullable result type and bump version to 0.3.4

--- src/mcp_jenkins/models/build.py
+++ src/mcp_jenkins/models/build.py
@@ -12,13 +12,12 @@ class Build(BaseModel):
     # The following fields are determined by the depth when get info
     name: str = None
     node: str = None
-    executor: int | None = None
 
     class_: str | None = Field(None, alias='_class')
     building: bool = None
     duration: int = None
     estimatedDuration: int = None
-    result: str = None
+    result: str | None = None
     timestamp: int = None
     inProgress: bool = None
     nextBuild: Optional['Build'] = None
